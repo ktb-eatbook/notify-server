8a7d62e84c82b004721700ff0528d3ddf8278b43
KangMins <rkdalsdl98@naver.com>
2024-12-24 19:24:41 +0900

    푸시 알림 개발 메시지 큐 x

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions : CorsOptions = {
-    origin: "https://ktb-book-keeper.netlify.app",
+    origin: ["http://localhost:5173","https://ktb-book-keeper.netlify.app"],
     methods: ["POST", "OPTIONS"],
     credentials: false,
     allowedHeaders: ["Authorization", "Content-Type"]
